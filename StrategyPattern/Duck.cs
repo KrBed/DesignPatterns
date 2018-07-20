879ee10d1f52ddaeab1550460496d4e3f145ab11
Krzysztof Bednarski <bednarski1978@gmail.com>
2018-07-20 06:59:12 +0200

    initial commit

---
+++ StrategyPattern/Duck.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    public abstract class Duck
+    {
+        IFlyBehaviour flyingBehaviour;
+        IQuackBehaviour quackingBehaviour;
+
+        public abstract void Display();
+
+        public void MakeFly()
+        {
+            flyingBehaviour.Fly();
+        }
+        public void MakeQuack()
+        {
+            quackingBehaviour.Quack();
+        }
+        public void SetFly(IFlyBehaviour flyBehaviour)
+        {
+            flyingBehaviour = flyBehaviour;
+        }
+        public void SetQuack(IQuackBehaviour quackBehaviour)
+        {
+            quackingBehaviour = quackBehaviour;
+        }
+
+    }
+}
