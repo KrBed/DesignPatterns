6c6adfadf86f9dba4d2166f02335dd27fbe66391
Krzysztof Bednarski <bednarski1978@gmail.com>
2018-07-22 22:49:56 +0200

    init

--- StrategyPattern/Program.cs
+++ StrategyPattern/Program.cs
@@ -10,6 +10,8 @@ namespace StrategyPattern
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
         }
     }
 }
